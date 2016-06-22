26113f76f459ed0c9dd040c8596a98044ae7d4cd
David Smith <shmish111@gmail.com>
2016-06-22 10:40:35 +0100

    bump to next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject degenerate "0.1.7"
+(defproject degenerate "0.1.8-SNAPSHOT"
   :description "Useful generators for test.check"
   :url "https://github.com/shmish111/degenerate"
   :license {:name "Eclipse Public License"
